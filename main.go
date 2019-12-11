1c5aee616a84d74677f979dcf629fc3a12370703
王伟 <745689249@qq.com>
2019-12-11 16:42:09 +0800

    clear code

--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 
 func main() {
 	models.Init()
-	beego.AddFuncMap("handleJS",handleJS)
-	beego.Run()
-}
 
-func handleJS(in string)(out string){
-	out = in + "world"
-	return
+	beego.Run()
 }
 

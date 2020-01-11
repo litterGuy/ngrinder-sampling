9495cb3f32222837669d9ddfe2ba11edcdffc061
王伟 <745689249@qq.com>
2020-01-11 11:39:50 +0800

    set if ding talk appid is empty,hidden the login type

--- controllers/default.go
+++ controllers/default.go
@@ -12,14 +12,17 @@ type MainController struct {
 
 func (c *MainController) Get() {
 	appId := beego.AppConfig.String("dingtalk.appid")
-	redirectUrl := c.Ctx.Input.Site() + "/dingTalk"
-	port := c.Ctx.Input.Port()
-	if port != 80 && port != 443 {
-		redirectUrl = c.Ctx.Input.Site() + ":" + strconv.Itoa(port) + "/dingTalk"
+	//如果不配置dingtalk，则隐藏页面相关信息
+	if len(appId) > 0 {
+		redirectUrl := c.Ctx.Input.Site() + "/dingTalk"
+		port := c.Ctx.Input.Port()
+		if port != 80 && port != 443 {
+			redirectUrl = c.Ctx.Input.Site() + ":" + strconv.Itoa(port) + "/dingTalk"
+		}
+		gotoUrl := "https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=" + appId + "&response_type=code&scope=snsapi_login&state=STATE&redirect_uri=" + redirectUrl
+		c.Data["goto"] = url.QueryEscape(gotoUrl)
+		c.Data["goto_original"] = gotoUrl
 	}
 
-	gotoUrl := "https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=" + appId + "&response_type=code&scope=snsapi_login&state=STATE&redirect_uri=" + redirectUrl
-	c.Data["goto"] = url.QueryEscape(gotoUrl)
-	c.Data["goto_original"] = gotoUrl
 	c.TplName = "login.html"
 }
